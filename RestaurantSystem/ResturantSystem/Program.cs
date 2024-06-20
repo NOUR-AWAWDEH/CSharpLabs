c20de3b9f80de01e9dea966fe58e6b3b6a04a273
NOUR-AWAWDEH <nour.awawdeh95@gmail.com>
2024-06-20 15:16:32 +0300

    New Task of RestaurantSystem

---
+++ RestaurantSystem/ResturantSystem/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+var reservationSystem = new ReservationSystem();
+
+Console.WriteLine(reservationSystem.AddReservation(1, "Alex", "18:00"));
+
+Console.WriteLine(reservationSystem.AddReservation(2, "Ahmed", "19:00"));
+
+//Should fail!!
+Console.WriteLine(reservationSystem.AddReservation(1, "Aristote", "18:00"));
+
+Console.WriteLine(reservationSystem.ViewReservations());
+
+Console.WriteLine(reservationSystem.CancelReservation(1, "18:00"));
+
+Console.WriteLine(reservationSystem.ViewReservations());
+ 
+/*
+My Approach To Solve the task :
+ 
+ 1. Add a Reservation:
+    • Input: AddReservation(1, "Alice", "18:00")
+    • Output: Reservation added for Alice at table 1 for 18:00.
+ 2. Cancel a Reservation:
+    • Input: CancelReservation(1, "18:00")
+    • Output: Reservation for table 1 at 18:00 has been canceled.
+ 3. View Reservations:
+    • Input: ViewReservations()
+    • Output:sqlCopy code  Current Reservations:
+        • Table 1: Alice at 18:00
+        • Table 2: Bob at 19:00
+ */
---
+++ RestaurantSystem/ResturantSystem/Reservation.cs
@@ -0,0 +1,21 @@
+﻿
+public class Reservation
+{
+	public int TableNumber {  get; set; }
+    public string  CustomerName { get; set; }
+	public string TimeSlot {  get; set; }
+
+    public Reservation(int tableNumber, 
+        string customerName, string timeSlot)
+    {
+		TableNumber = tableNumber;
+		CustomerName = customerName;
+		TimeSlot = timeSlot;
+	}
+
+	public override string ToString()
+	{
+		return $"Table {TableNumber}:{CustomerName} at " +
+			$"{TimeSlot}";
+	}
+}
